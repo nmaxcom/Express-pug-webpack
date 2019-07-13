de8cdf76398ea973c34c2c4389817a9c419fa655
Carles Alcolea <nmaxcom@gmail.com>
2019-07-13 09:16:56 +0200

    Dandole forma

--- app.js
+++ app.js
@@ -1,48 +1,84 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+/**
+ *  Plan:
+ *    1) Simple Express app with dev workflow (Webpack etc)
+ *    2) Fancy stuff
+ *    3) Security
+ *    4) Optimization
+ *
+ *    Gold: https://github.com/i0natan/nodebestpractices
+ *
+ *    (discarded) Use dotenv! Configure dev and prod
+ *  TODO:
+ *    Add good error handling
+ *    Compartimentalize server
+ *    Use **router.use**('/v1', v1ApiController) to set up routing
+ *    Serve all static files from S3
+ *    Upload content (photos from blog etc) to S3 as well
+ *    Notify me of ANY type of error (server down, 4**, 5**, Error thrown etc.)
+ *    Use a great logger like Winston (https://github.com/winstonjs/winston/tree/master/examples), Bunyan (highly popular) or Pino
+ *    Prevent bruteforce attacks https://www.npmjs.com/package/rate-limiter-flexible
+ *
+ */
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const chalk = require('chalk');
+const dotenv = require('dotenv');
+const flash = require('express-flash');
+const passport = require('passport');
+const session = require('express-session');
+const errorhandler = require('errorhandler');
 
-var app = express();
-
-// view engine setup
+const isProduction = process.env.NODE_ENV === 'production';
+/**
+ * View engine
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+/**
+ * Various middleware
+ */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+                         src:            path.join(__dirname, 'public'),
+                         dest:           path.join(__dirname, 'public'),
+                         indentedSyntax: true, // true = .sass and false = .scss
+                         sourceMap:      true,
+                       }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * Routing
+ */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+/**
+ * Error Handling
+ */
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+if (!isProduction) {
+  // only use in development
+  app.use(errorhandler());
+} else {
+  app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Server Error');
+  });
+}
 
 module.exports = app;

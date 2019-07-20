0e838507fd38ae981432de1edcfee7ddb68bb4bb
Carles Alcolea <nmaxcom@gmail.com>
2019-07-20 17:56:43 +0200

    Buggy version

--- index.js
+++
@@ -1,1 +0,0 @@
-// Not being used ATM'use strict';const server = require('./server')();// const config = require('./config');const db = require('./config');const lala = require('./config');server.create(config, db);server.start();
